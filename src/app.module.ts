015dd945f815f5030a25d220ef75e25df65c94c0
wangmeng <wangmeng08@sparkedu.com>
2023-11-28 16:54:44 +0800

    feat: initial user and login

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoginModule } from './login/login.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [LoginModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
