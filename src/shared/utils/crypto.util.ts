29a671a7fadf66cae291a938af980ce0364a2e02
wangmeng <wangmeng08@sparkedu.com>
2023-12-08 11:14:21 +0800

    fix: some comment

--- src/shared/utils/crypto.util.ts
+++ src/shared/utils/crypto.util.ts
@@ -1,12 +1,8 @@
-/**
- * crypto 常用封装方法
- */
-
 import * as crypto from 'crypto';
 
 /**
- * 随机盐
- * @param len 长度
+ * Random salt
+ * @param len
  * @returns
  */
 export function makeSalt(len = 3): string {
@@ -14,22 +10,21 @@ export function makeSalt(len = 3): string {
 }
 
 /**
- * 使用盐加密明文密码
- * @param password 密码
- * @param salt 密码盐
+ * encrypt password
+ * @param password
+ * @param salt
  */
 export function encryptPassword(password: string, salt: string): string {
   if (!password || !salt) {
     return '';
   }
   const tempSalt = Buffer.from(salt, 'base64');
-  return (
-    // 10000 代表迭代次数 16代表长度
-    crypto.pbkdf2Sync(password, tempSalt, 10000, 16, 'sha1').toString('base64')
-  );
+  return crypto
+    .pbkdf2Sync(password, tempSalt, 10000, 16, 'sha1')
+    .toString('base64');
 }
 
-// 计算 流 MD5
+// compute md5
 export function encryptFileMD5(buffer: Buffer) {
   const md5 = crypto.createHash('md5');
 
